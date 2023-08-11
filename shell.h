ccc8bfc5efe5bad8046329dc67091868773b3f71
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-08-11 16:37:11 -0700

    Split find path function into two to be betty compliant.

--- shell.h
+++ shell.h
@@ -15,10 +15,11 @@ extern char **environ;
 void getline_inp(char **buffer);
 void env_builtin(char *buffer, char **envp);
 char **tokens(char *buffer);
-char *find_path(const char *cmd);
 void exit_func(char *buffer);
 void call_fork(char *buffer, char **args, char *command_path);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *_strtok(char *str, const char *delim);
+char *find_path_env(void);
+char *find_executable_path(const char *cmd);
 
 #endif /* SHELL_H */
