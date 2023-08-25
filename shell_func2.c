2a1c9ab2d6acb7c8b81e9165282d21eb7a0b027d
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-08-25 14:22:30 -0700

    Updated fork function.

--- shell_func2.c
+++ shell_func2.c
@@ -1,5 +1,50 @@
 #include "shell.h"
 
+/**
+ * call_fork - calls the fork function
+ *
+ * @buffer: contains the command
+ * @args: tokenized buffer
+ * @command_path: contains the path
+ *
+ * Return: WEXITSTATUS(status)
+ */
+int call_fork(char *buffer, char **args, char *command_path)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error creating a child process");
+		free(buffer);
+		free(command_path);
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execve(command_path, args, NULL);
+		perror("Error executing command");
+		free(buffer);
+		free(command_path);
+		free(args);
+		exit(127);
+	}
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		perror("Error while waiting");
+		free(buffer);
+		free(command_path);
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else
+		return (-1);
+}
 
 /**
  * cd_builtin - Handles the "cd" command
